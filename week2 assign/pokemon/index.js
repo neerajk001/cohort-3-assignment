3321cf73ea45652885dda6b3d879897451ba4873
neeraj kushwaha <workwithneeraj.01@gmail.com>
2024-09-25 23:59:06 +0530

    Revert "the main fn added"
    This reverts commit b9d8587577f6bfdac4657cd20634b6858e4ed284.

--- week2 assign/pokemon/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('pokemon-form').addEventListener('submit',function(event){
-    event.preventDefault();
-
-    const numCards =document.getElementById('num-cards').value 
-    const category =document.getElementById('category').value
-
-    fetchPokemons( numCards, category)
-
-})
