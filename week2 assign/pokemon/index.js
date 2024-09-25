b9d8587577f6bfdac4657cd20634b6858e4ed284
neeraj kushwaha <workwithneeraj.01@gmail.com>
2024-09-25 23:30:52 +0530

    the main fn added

---
+++ week2 assign/pokemon/index.js
@@ -0,0 +1,9 @@
+document.getElementById('pokemon-form').addEventListener('submit',function(event){
+    event.preventDefault();
+
+    const numCards =document.getElementById('num-cards').value 
+    const category =document.getElementById('category').value
+
+    fetchPokemons( numCards, category)
+
+})
