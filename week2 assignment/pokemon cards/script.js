a378f3589c49796f775557ef00705e0ab54fa51e
neeraj kushwaha <workwithneeraj.01@gmail.com>
2024-09-26 08:21:37 +0530

    main submit fn

--- week2 assignment/pokemon cards/script.js
+++ week2 assignment/pokemon cards/script.js
@@ -2,5 +2,9 @@ document.getElementById("pokemon-form").addEventListener('submit',function(event
     event.preventDefault();
 
     const numCards =document.getElementById('num-cards').value 
-    
+    const category =document.getElementById('category').value 
+
+    fetchPokemons( numCards ,category)
+
+
 })
